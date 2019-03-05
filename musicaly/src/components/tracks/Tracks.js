9c2f76525ca63d0460907bd75d22d7ed57165a2a
Vlad <vovaturcan4@gmail.com>
2019-03-05 17:24:08 +0200

    fetched successfully the data from the api and styled the main showing page

--- musicaly/src/components/tracks/Tracks.js
+++ musicaly/src/components/tracks/Tracks.js
@@ -1,13 +1,28 @@
 import React, { Component } from "react";
 import { Consumer } from "../../context";
+import Spinner from "../layout/Spinner";
+import Track from "./Track";
 
 export default class Tracks extends Component {
   render() {
     return (
       <Consumer>
         {value => {
-          console.log(value);
-          return <h1>Hi</h1>;
+          const { track_list, heading } = value;
+          if (track_list.length === 0 || track_list === undefined) {
+            return <Spinner />;
+          } else {
+            return (
+              <>
+                <h3 className="text-center mb-4">{heading}</h3>
+                <div className="row">
+                  {track_list.map(track => (
+                    <Track track={track.track} key={track.track.track_id} />
+                  ))}
+                </div>
+              </>
+            );
+          }
         }}
       </Consumer>
     );
